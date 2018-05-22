c9f14b727c2491a1e1549c8b68facd2b80a2b8f0
Yana <Yana@DESKTOP-G09V093>
2018-05-22 04:30:11 +0300

    add

--- HWbuttons/Form1.cs
+++ HWbuttons/Form1.cs
@@ -16,5 +16,22 @@ namespace HWbuttons
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_MouseMove(object sender, MouseEventArgs e)
+        {
+            button1.Location = new Point(
+                e.X - 10, e.Y -10
+                );
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
